6e7aa4fd0f53d7173dd217e79aaa5bc53d1bb4bb
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-12 20:54:34 +0100

    Create +page.svelte

---
+++ src/routes/3-svelte-inview/4-animations/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { inview } from "svelte-inview";
+  let videoRef: HTMLVideoElement;
+</script>
+
+<h1>Animations</h1>
+
+<p>
+  You can also add some cool animations when an element enters the viewport. To
+  make sure the animation won't fire too soon you can pass a negative value to
+  rootMargin. When inView is true, add an animation class to your target.
+  Additionally, you can detect the scroll direction to make the animations even
+  cooler!
+</p>
+<hr />
+
+<div style="height: 900px; width:50px"></div>
+<div use:inview on:inview_leave={() => videoRef.pause()}>
+  <video width="500" controls bind:this={videoRef} poster="/poster.png">
+    <source src="/test.mp4" type="video/mp4" />
+    <track kind="captions" />
+  </video>
+</div>
